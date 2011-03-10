0daa57fd2829887a70fc15c7af8cc62f85a06637
Frank Bille <watzy@frankbille.dk>
2011-03-10 17:48:11 +0100

    Upgraded Jetty to latest version

--- yatzy-frontend/src/test/java/org/apache/wicket/examples/yatzy/JettyStarter.java
+++ yatzy-frontend/src/test/java/org/apache/wicket/examples/yatzy/JettyStarter.java
@@ -1,14 +1,14 @@
 package org.apache.wicket.examples.yatzy;
 
-import org.mortbay.jetty.Server;
-import org.mortbay.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 public class JettyStarter {
 
 	public static void main(String[] args) throws Exception {
 		// System.setProperty("wicket.configuration", "development");
 		Server server = new Server(8080);
-		server.addHandler(new WebAppContext("src/main/webapp", "/"));
+		server.setHandler(new WebAppContext("src/main/webapp", "/"));
 		server.start();
 	}
 
