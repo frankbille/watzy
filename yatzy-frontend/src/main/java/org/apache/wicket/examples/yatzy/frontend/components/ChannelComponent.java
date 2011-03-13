ec3fafcf3ed50c793aa28bf9f3fc176ac869b501
Frank Bille <watzy@frankbille.dk>
2011-03-13 16:42:06 +0100

    Included the channel name in the update event and added some documentation to the class

--- yatzy-frontend/src/main/java/org/apache/wicket/examples/yatzy/frontend/components/ChannelComponent.java
+++ yatzy-frontend/src/main/java/org/apache/wicket/examples/yatzy/frontend/components/ChannelComponent.java
@@ -12,32 +12,66 @@ import com.google.appengine.api.channel.ChannelMessage;
 import com.google.appengine.api.channel.ChannelService;
 import com.google.appengine.api.channel.ChannelServiceFactory;
 
+/**
+ * Component that links Apache Wicket and Google App Engines Channel API. It
+ * uses Wickets inter-component events, which were introduced in version 1.5.
+ * 
+ * Currently it does 3 trips from client to server when an update is to be
+ * pushed to the clients.
+ * 
+ * <ol>
+ * <li>Server->Client. The server tells the client(s) that an update is
+ * available on the server. (LIGHTWEIGHT)
+ * <li>Client->Server. The client(s) initializes an AJAX request to the server
+ * to instantiate the {@link AjaxRequestTarget} on the server. (LIGHTWEIGHT)
+ * <li>Server->Client. On the server sends an event to everyone interested that
+ * an update has happened on a given channel. An instance of
+ * {@link AjaxRequestTarget} is attached to the event, which consumers can then
+ * use to do what they want. (LIGHTWEIGHT->HEAVYWEIGHT depending on how much the
+ * constumers want to update.)
+ * </ol>
+ * 
+ * In theory it should be possible to hook into Wickets request flow and gather
+ * what components want to do before making the first trip. This would eliminate
+ * trip 2 and 3. This option hasn't been explored yet.
+ * 
+ * @see http://code.google.com/appengine/docs/java/channel/overview.html
+ * @see https
+ *      ://cwiki.apache.org/WICKET/migration-to-wicket-15.html#MigrationtoWicket1
+ *      .5-Intercomponentevents
+ */
 public class ChannelComponent extends Panel {
 	private static final long serialVersionUID = 1L;
 	
 	public static final ChannelUpdateEventPayload TRIGGER_UPDATE_EVENT = new ChannelUpdateEventPayload();
 	
 	public static final class ChannelUpdatedPayload {
+		private String channelName;
 		private AjaxRequestTarget target;
 
-		public ChannelUpdatedPayload(AjaxRequestTarget target) {
+		public ChannelUpdatedPayload(String channelName, AjaxRequestTarget target) {
+			this.channelName = channelName;
 			this.target = target;
 		}
 
 		public AjaxRequestTarget getAjaxRequestTarget() {
 			return target;
 		}
+		
+		public String getChannelName() {
+			return channelName;
+		}
 	}
 	
 	private static final class ChannelUpdateEventPayload {
 	}
 	
-	private static class AjaxCallback extends AbstractDefaultAjaxBehavior {
+	private class AjaxCallback extends AbstractDefaultAjaxBehavior {
 		private static final long serialVersionUID = 1L;
 
 		@Override
 		protected void respond(AjaxRequestTarget target) {
-			getComponent().send(YatzyApplication.get(), Broadcast.BREADTH, new ChannelUpdatedPayload(target));
+			send(YatzyApplication.get(), Broadcast.BREADTH, new ChannelUpdatedPayload(channelName, target));
 		}
 		
 		@Override
